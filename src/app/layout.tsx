03c15093fb6e076bff609abfc7ca1048c5d32ee0
luizdevjava <primeiragp@proton.me>
2025-12-10 15:35:14 -0300

    Add files via upload
    1234567

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import { SessionProvider } from "next-auth/react";
+import { Providers } from "@/components/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,10 +41,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
-        <SessionProvider>
+        <Providers>
           {children}
           <Toaster />
-        </SessionProvider>
+        </Providers>
       </body>
     </html>
   );
